0223a3b2408204e98cb1b3e1164b00e89af7d9bb
Hamid Javadi <javadi.business@gmail.com>
2022-04-30 12:04:32 +0430

    update project structure

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import logo from '../../assets/image/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Let's start to develop a react app like Pinterest... </h1>
+      <img src={logo} />
+    </div>
+  );
+}
+
+export default App;
