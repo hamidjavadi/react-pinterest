0ba7151301e88d21b8888dafc44f2d221830dad7
Hamid Javadi <javadi.business@gmail.com>
2022-05-12 13:17:26 +0300

    - Install eslint - Install prettier

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,11 @@
-import logo from "../../assets/image/logo.svg"
-import './app.scss';
+import logo from "../../assets/image/logo.svg";
+import "./app.scss";
 
 function App() {
   return (
     <div>
       <h1>Let's start to develop a react app like Pinterest... </h1>
-      <img src={logo} />
+      <img src={logo} alt="react-logo" />
     </div>
   );
 }
