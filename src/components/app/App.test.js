67bfad132521e0803dec13764ad3a8c65ab872af
Hamid Javadi <javadi.business@gmail.com>
2022-05-15 17:16:37 +0300

    Add app component test

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./app";
 
-test("renders learn react link", () => {
+test("Render Title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByTestId(/title/i);
+  expect(title).toHaveTextContent("Pinterest");
 });
